66ea6e8915b61954681e7a4ae5a0a252dd22f343
xandowski <xandy-moraes@hotmail.com>
2025-05-04 18:45:12 -0300

    adicionado meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-// src/app/layout.tsx
 import type { Metadata } from 'next'
 import { ClientProviders } from '../components/ClientProviders'
 import '../global.css'
@@ -24,6 +23,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="icon" href="/icon.ico" />
         <link rel="manifest" href="/manifest.webmanifest" />
         <link rel="apple-touch-icon" href="/apple-icon.png" />
+        <meta name="viewport" 
+              content="width=device-width, initial-scale=1, viewport-fit=cover" 
+      />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
       <body>
         <ClientProviders>{children}</ClientProviders>
