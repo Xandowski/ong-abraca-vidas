ba44127db2aecdd92d8903fe924b437d91ae1d8c
xandowski <xandy-moraes@hotmail.com>
2025-05-04 17:37:06 -0300

    fix calendar component

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { DayPicker } from "react-day-picker";
+import * as React from "react";
+import { DayPicker, } from "react-day-picker";
 
-import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
@@ -54,7 +54,7 @@ function Calendar({
       components={{
         IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
-      }}
+      }as unknown as React.ComponentProps<typeof DayPicker>["components"]}
       {...props}
     />
   );
@@ -62,3 +62,4 @@ function Calendar({
 Calendar.displayName = "Calendar";
 
 export { Calendar };
+
